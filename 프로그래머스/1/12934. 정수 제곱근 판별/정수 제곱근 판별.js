1687cf10110a6e2ca5b2d251ba5231c4ba4fe17e
yoonhyeonjeong <66927252+yoonhyeonjeong@users.noreply.github.com>
2024-10-22 16:44:46 +0900

    Update 정수 제곱근 판별.js

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -3,7 +3,6 @@ function solution(n) {
     for(let i = 1; i<= n; i++){
         if(n != i * i){ // 제곱근이 아니면 
             answer = -1
-            continue;
         }else{ // 제곱근이면
             answer = (i+1) * (i+1) 
             break;
